adabf1add59a426fc6996219d6e158682fb4df7d
Mohammed Yasin Vohra <115548584+myv04@users.noreply.github.com>
2025-03-27 00:12:34 +0000

    deleting all files

--- static/student_modules.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const moduleCards = document.querySelectorAll(".module-card");
-
-    moduleCards.forEach(card => {
-        // Remove the click event on the whole module card
-        card.style.cursor = "default"; // Ensures it doesn’t look clickable
-    });
-});
